834c0de2b07b2b6772dd2b779d9aa14008cb3820
bansariakhani18 <202512032@dau.ac.in>
2026-07-19 02:42:20 +0530

    Inspect libsql API

--- backend/app/turso_db.py
+++ backend/app/turso_db.py
@@ -5,12 +5,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+print("LIBSQL MODULE:", libsql)
+print("HAS Row:", hasattr(libsql, "Row"))
+
+if hasattr(libsql, "Row"):
+    print("Row object:", libsql.Row)
+
 conn = libsql.connect(
     "/tmp/test.db",
     sync_url=os.getenv("TURSO_DATABASE_URL"),
     auth_token=os.getenv("TURSO_AUTH_TOKEN")
 )
 
-print("row_factory =", getattr(conn, "row_factory", None))
+print("DEFAULT row_factory:", conn.row_factory)
 
 sys.exit(0)
